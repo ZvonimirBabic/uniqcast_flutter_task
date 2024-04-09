3974fff725db5e2ebef0bee0b708060f72ef6822
BlackWW <zvobabic@hotmail.com>
2024-04-09 22:16:23 +0200

    Add: Alice service & Logger Service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uniqcast_flutter_task/services/alice_service.dart';
 import 'package:uniqcast_flutter_task/uniqcast_flutter_task_app.dart';
+
+import 'services/logger_service.dart';
 
 ///
 /// Function first called when running our project
@@ -7,6 +11,15 @@ import 'package:uniqcast_flutter_task/uniqcast_flutter_task_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// Initialize [Logger] and [Alice]
+  Get
+    ..put(
+      LoggerService(),
+    )
+    ..put(
+      AliceService(),
+    );
 
   /// Run the `Uniqcast flutter task` app
   runApp(
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,4 +1,4 @@
-import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get.dart';
 import 'package:uniqcast_flutter_task/screens/login/login_screen.dart';
 
 import '../screens/login/login_binding.dart';
